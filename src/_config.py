38c3c7ac18e75794c9be20c0910d92698c501149
hschickdevs <64290193+hschickdevs@users.noreply.github.com>
2022-04-25 01:56:52 -0500

    Update _config.py

--- src/_config.py
+++ src/_config.py
@@ -13,7 +13,8 @@ DISCORD_BOT_TOKEN: str = os.getenv('DISCORD_BOT_TOKEN', 'your_discord_bot_token'
 assert DISCORD_BOT_TOKEN != 'your_discord_bot_token'
 
 # Input the channel IDs that you want the bot to post IPO alerts in, no-space comma delimited:
-DISCORD_BOT_CHANNEL_IDS: list = [int(_id.strip()) for _id in os.getenv('DISCORD_BOT_CHANNEL_IDS', '1234,1234').split(",")]
+DISCORD_BOT_CHANNEL_IDS: list = [int(_id.strip()) for _id in
+                                 os.getenv('DISCORD_BOT_CHANNEL_IDS', '1234,1234').split(",")]
 assert DISCORD_BOT_CHANNEL_IDS != [1234, 1234]
 
 # The time period for checking IPO statuses (in seconds)
